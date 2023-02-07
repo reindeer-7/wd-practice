df5325f2096269b2d21887c97ba797349aa625f8
Joseph Ramos <jardix22@gmail.com>
2023-02-06 20:54:44 -0500

    Add JavaScript initial files.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const year = 2023; // Number
+const name = "Toddy"; // String
+const isOlder = true; // Boolean true | false
+
+// console.log(name)
+
+// Arrays     [0, 1]
+const names = ["Julian", "Mbappe"];
+const ages = [32, 15, 10];
+const agesAndNames = [32, 15, 10, "Hola", false];
+
+
+// console.log(ages[2])
+
+// Objects
+const student = {
+  name: "Juan",
+  age: 56,
+  isOlder: true,
+  pokemons: ["Pikachu", "Charmander"],
+};
+
+// console.log(student.pokemons)
+
+// Function definition
+function sum(a, b) {
+  return a + b;
+}
+
+
+// Call function
+// console.log(sum(10, 5))
+
+const myFunction = function (name) {
+  return "Hola " + name + "!";
+}
+
+console.log(myFunction("Pepito"))
+
+
