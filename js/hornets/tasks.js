8015f5c0d7af2c9452463e5cb521dcfee7276ef9
Joseph Ramos <jardix22@gmail.com>
2023-02-13 21:05:34 -0500

    Add object examples.

--- js/hornets/tasks.js
+++ js/hornets/tasks.js
@@ -1,14 +1,123 @@
-// Crear una variable almacenar el apellido de una persona.
+// Yuli: Crear una variable almacenar el nombre de una persona "Pulpin".
+// Lenin: Crear una variable almacenar la edad una persona 15.
+// Henry: Crear una variable almacenar el DNI de persona 45612378.
+// Paul: Crear una variable almacenar el n'umero de pares de zapatos i.e 6.
+// Yesica: Crear una variable almacenar si es un n'umero positivo i.e true/false.
+
+
+// Crear un objeto para almacenar la informaci'on de una persona
+// como: nombre, apellidos, edad, estado civil y si tiene licencia de conducir.
+// Luis Perez Garcia, 45, soltero, si tiene licencia.
+
+const personalData = {
+  name: "Luis"
+};
+
+const person = {
+  name: 'Paul'
+};
+
+
+const personalInformation = {
+  name: "Raul",
+  lastName: 'Romero Vargas',
+  age: 23,
+  isSingle: false,
+  hasDriverLicense: true,
+  child: {
+    name: "Luis",
+    edad: 10,
+    isSingle: true,
+    hasDriverLicense: false,
+    pokemons: [ "Pikachu", "Charmander" ],
+  }
+}
+
+const links = [
+  { label: "Home", url: "/" },
+  { label: "News", url: "/news" }
+];
+
 // Crear una variable para almacenar el numero de botellas de una caja de cerveza.
 // Crear una variable para almacenar el nombre de mi mascota "Pelusa".
 // Crear una variable para almacenar el numero pelotas de volley que tengo en casa.
 
 // Crear una variable para almacenar la edad de su herman@.
 
-// Crear un array de 3 numeros impares.
-// Crear un array de 2 numeros pares.
-// Crear un array de 3 nombres de mascotas.
-// Crear un array de 3 edades.
+// Henry: Crear un array de 3 numeros impares.
+const oddNumbers = [1, 3, 5];
 
+// Lenin: Crear un array de 2 numeros pares.
+const evenNumbers = [2, 4];
+
+// Yuli: Crear un array de 3 nombres de mascotas.
+const pets = ["Luz" , "Moon"];
+
+// Paul: Crear un array de 3 edades.
+const agePeople = [24, 46, 79];
+
+// Yesica: Crear un array de dias de la semana (usar iniciales i.e LU).
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+// Juan: Crear un array con los nombres de las iglesias de Juli.
 // Crear un objeto para almacenar la informaci'on de una persona
 // como: nombre, apellidos, edad y estado civil.
+
+const pokemonInformation = {
+  name: 'Pikachu',
+  number: '0025',
+  description: 'Cuando se enfada, este Pokémon descarga la energía que alamacena en el interior de las bolsas de las mejillas',
+  height: 0.4,
+  category: 'ratón',
+  weight: 6.0,
+  hability: 'Elec. Estática',
+  type: 'Eléctrico',
+  weaknesses: ['Tierra', 'Eléctrico'],
+}
+
+const pokemon = {
+  name: "Pikachu",
+  number: 0025,
+  description: "Cuando se enfada, este Pokémon descarga la energía que almacena en su interior",
+  height: 0.4,
+  category: "Raton",
+  weight: 6.0,
+  ability: "Elec. Estática",
+  type: "Eléctrico",
+  weaknesses: ['Tierra', 'Eléctrico'],
+};
+
+const pokemon2 = {
+  name: 'Pikachu',
+  number: 0025,
+  description: 'Cuando se enfada...',
+  height: 0.4,
+  category: 'Ratón',
+  weight: 6.0,
+  ability: 'Elec. Estática',
+  type: 'Eléctrico',
+  weaknesses: ['Elécttrico', 'Tierra'],
+};
+
+const Pikachu = {
+  names: "Pikachu",
+  number: "0025",
+  information: "Cuando se enfada, este Pokémon descarga la energía que alamacena en el interior de las bolsas de las mejillas",
+  height: 0.4,
+  category: "Raton",
+  weight: 6.0,
+  ability: "electric estatica",
+  types: ["Electrico", "Volador"],
+  weakness: ["Tierra" ,"Electrico"]
+}
+
+
+const pikachu = {
+  id: 0025,
+  description: "...",
+  height: '0.4 m',
+  weight: '6.0 kg',
+  skill: 'static electricity',
+  type: 'electric',
+  weaknesses: ['electric', 'ground']
+}
--- js/hornets/yesica.js
+++ js/hornets/yesica.js
@@ -14,3 +14,29 @@ const petName = "Pelusa";
 
 // Crear una variable para almacenar el numero pelotas de volley que tengo en casa.
 let ballNumbers;
+
+
+// Crear una variable para almacenar si es un numero positivo 'true, false'
+const evenNumber = true;
+
+const personalInformation = {
+  name: 'Raul',
+  lastName: 'Romero Vargas',
+  age: 23,
+  isSingle: false,
+  hasDriverLicense: true,
+}
+
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const pokemon = {
+  name: 'Pikachu',
+  id: '0025',
+  description: 'Cuando se enfada...',
+  height: 0.4,
+  category: 'Ratón',
+  weight: 6.0,
+  ability: 'Elec. Estática',
+  type: 'Eléctrico',
+  weaknesses: ['Elécttrico', 'Tierra'],
+};
