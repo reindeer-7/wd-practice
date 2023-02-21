f8a46ae7186f619d8b3e0592859ac0b3ff96bd54
Joseph Ramos <jardix22@gmail.com>
2023-02-21 18:06:03 -0500

    Add object example.

---
+++ js/objects.js
@@ -0,0 +1,36 @@
+const poke = {
+  "id": 1,
+  "name": {
+    "english": "Bulbasaur",
+    "japanese": "フシギダネ",
+    "chinese": "妙蛙种子",
+    "french": "Bulbizarre"
+  },
+  "type": ["Grass", "Poison"],
+  "base": {
+    "HP": 45,
+    "Attack": 49,
+    "Defense": 49,
+    "Sp. Attack": 65,
+    "Sp. Defense": 65,
+    "Speed": 45
+  },
+  "species": "Seed Pokémon",
+  "description": "Bulbasaur can be seen napping ...",
+  "evolution": { "next": [["2", "Level 16"]] },
+  "profile": {
+    "height": "0.7 m",
+    "weight": "6.9 kg",
+    "egg": ["Monster", "Grass"],
+    "ability": [
+      ["Overgrow", "false"],
+      ["Chlorophyll", "true"]
+    ],
+    "gender": "87.5:12.5"
+  },
+  "image": {
+    "sprite": "https://raw.githubusercontent.com",
+    "thumbnail": "https://raw.githubusercontent.com",
+    "hires": "https://raw.githubusercontent.comg"
+  }
+};
